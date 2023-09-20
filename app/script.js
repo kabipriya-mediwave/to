5c70f1ffdba33916597be6cceb7e815c4667ba1a
kabipriya-mediwave <kabipriya@mindwaveventures.com>
2023-09-20 11:20:38 +0530

    added array

--- app/script.js
+++ app/script.js
@@ -1,71 +1,110 @@
-let button = document.querySelector("#add");
-let List = document.querySelector("#List");
-let input = document.querySelector("#input");
-
-let todos = [];
-
-button.addEventListener("click", () => {
-  todos.push(input.value);
-  saveToLocalStorage();
-  updateListUI();
-  console.log("yes");
-  addtodo(input.value);
-  clear();
-});
-
-function addtodo(todo) {
-  let div = document.createElement("div");
-
-  let checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-  let li = document.createElement("label");
-  li.innerText = todo;
-  div.appendChild(checkbox);
-  div.appendChild(li);
-
-  checkbox.addEventListener("change", function () {
-    if (checkbox.checked) {
-      const completed = document.getElementById("completed");
-      li.style.textDecoration = "line-through";
-      completed.appendChild(div);
-    } else {
-      const uncompleted = document.getElementById("List");
-      li.style.textDecoration = "";
-      uncompleted.appendChild(div);
-    }
-  });
-  return div;
+allTasks = [];
+function MakeLiList(task) {
+  if (task.ischecked) {
+    const li = document.createElement("li");
+    li.setAttribute("id", `task-${task["id"]}`);
+    const inputCheckBox = document.createElement("input");
+    inputCheckBox.setAttribute("type", "checkbox");
+    inputCheckBox.checked = true;
+    let id = `task-${task["id"]}-check`;
+    inputCheckBox.setAttribute("id", id);
+    const p = document.createElement("p");
+    p.setAttribute("class", "para");
+    p.setAttribute("id", `task-${task["id"]}-para`);
+    p.innerHTML = task["do"];
+    p.style.textDecoration = "line-through";
+    li.appendChild(inputCheckBox);
+    li.appendChild(p);
+    inputCheckBox.addEventListener("change", function () {
+      if (inputCheckBox.checked) {
+        checkingCheckBox(task["id"], true);
+      } else {
+        checkingCheckBox(task["id"], false);
+      }
+    });
+    return li;
+  } else {
+    const li = document.createElement("li");
+    li.setAttribute("id", `task-${task["id"]}`);
+    const inputCheckBox = document.createElement("input");
+    inputCheckBox.setAttribute("type", "checkbox");
+    let id = `task-${task["id"]}-check`;
+    inputCheckBox.setAttribute("id", id);
+    const p = document.createElement("p");
+    p.setAttribute("class", "para");
+    p.setAttribute("id", `task-${task["id"]}-para`);
+    p.innerHTML = task["do"];
+    p.style.textDecoration = "";
+    li.appendChild(inputCheckBox);
+    li.appendChild(p);
+    inputCheckBox.addEventListener("change", function () {
+      if (inputCheckBox.checked) {
+        checkingCheckBox(task["id"], true);
+      }
+    });
+    return li;
+  }
 }
-function clear() {
-  let input = document.getElementById("input");
-  input.value = "";
+function updateUiList() {
+  clearUl();
+  for (let task of allTasks) {
+    const event = MakeLiList(task);
+    const unc = document.querySelector(".unchecked-task");
+    unc.appendChild(event);
+  }
+}
+function clearUl() {
+  const unc = document.querySelector(".unchecked-task");
+  unc.innerHTML = "";
+}
+function checkingCheckBox(taskId, bool) {
+  const checkIndex = allTasks.findIndex((task) => task.id == taskId);
+  if (checkIndex != -1) {
+    allTasks[checkIndex]["ischecked"] = bool;
+    sortArray();
+    saveToLocalStorage();
+    updateUiList();
+  }
+}
+function sortArray() {
+  allTasks.sort((a, b) =>
+    a.ischecked === b.ischecked ? 0 : a.ischecked ? 1 : -1
+  );
+}
+function addForm() {
+  const form = document.querySelector("#add-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    let Input = document.querySelector("#task").value;
+    let button = document.querySelector("#Add");
+    const task = {
+      id: new Date().getTime(),
+      do: Input,
+      ischecked: false,
+    };
+    addtask(task);
+    updateUiList();
+  });
+}
+function addtask(task) {
+  allTasks.push(task);
+  sortArray();
+  saveToLocalStorage();
 }
 
 function saveToLocalStorage() {
-  const str = JSON.stringify(todos);
-  localStorage.setItem("to-do-list", str);
+  const str = JSON.stringify(allTasks);
+  localStorage.setItem("my-event-list", str);
 }
-
 function getFromLocalStorage() {
-  const str = localStorage.getItem("to-do-list");
+  const str = localStorage.getItem("my-event-list");
   if (!str) {
-    todos = [];
+    return allTasks;
   } else {
-    todos = JSON.parse(str);
-  }
-}
-function clearApp() {
-  const app = document.querySelector("#List");
-  app.innerHTML = "";
-}
-
-function updateListUI() {
-  clearApp();
-  for (let i = 0; i < todos.length; i++) {
-    const todo = addtodo(todos[i]);
-    List.appendChild(todo);
+    allTasks = JSON.parse(str);
   }
 }
 
 getFromLocalStorage();
-updateListUI();
+updateUiList();
+addForm();
